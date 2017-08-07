4c0ad45799c619586e867137aff0b00f065fee3c
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-07 12:13:35 -0500

    registered new fns with to object

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ module.exports = {
     function: require('./function'),
     integer: require('./integer'),
     length: require('./length'),
+    lowerCase: require('./lower-case'),
     number: require('@timelaps/hacks/to-number'),
     object: require('./object'),
     path: require('./path'),
     string: require('./string'),
+    upperCase: require('./upper-case'),
 };
